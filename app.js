03c2282095929638d3ebb9a44642ec7fdb1299f8
nicolafelice <felicenicola99@hotmail.com>
2021-09-07 17:16:11 +0200

    add: sort class list by last name

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+//sort the class by last name
+function sortListOFStudents(arrayOfObjects) {
+
+    let listOfSurnamesClass = [];
+
+    for ( let i = 0; i < arrayOfObjects.length; i++ ) {
+        listOfSurnamesClass.push( arrayOfObjects[i].cognome );
+    }
+
+    listOfSurnamesClass.sort();
+
+    let newArrayOfObjects = [];
+    let idList = [];
+
+
+    for ( let e = 0; e < listOfSurnamesClass.length; e++ ) {
+        
+        for ( let i = 0; i < arrayOfObjects.length; i++ ) {
+
+            if( arrayOfObjects[i].cognome == listOfSurnamesClass[e] && !idList.includes(arrayOfObjects[i].id) ) {
+                newArrayOfObjects.push(arrayOfObjects[i]);
+                idList.push(arrayOfObjects[i].id);
+            }
+        }
+    }
+
+    return newArrayOfObjects;
+}
+
+
+function printListOfStudents(array) {
+    document.querySelector("#listOfStudents").innerHTML = ``;
+
+    for ( let i = 0; i < array.length; i++ ) {
+        document.querySelector("#listOfStudents").innerHTML += `<li class="student">${array[i].cognome} ${array[i].nome}</li>`;
+    }
+}
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
@@ -17,41 +54,53 @@ for ( let key in studente ) {
 
 let classe = [
     {
+        id : 0,
         nome : "mario",
         cognome : "rossi",
         eta : "35"
     },
     {
+        id : 1,
         nome : "claudio",
         cognome : "bianchi",
         eta : "25"
     },
     {
+        id : 2,
         nome : "ettore",
         cognome : "verdi",
         eta : "30"
     },
     {
+        id : 3,
         nome : "caterina",
         cognome : "bianchi",
         eta : "22"
     }
 ];
 
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-
-for ( let i = 0; i < classe.length; i++ ) {
-
-    document.querySelector("#listOfStudents").innerHTML += `<li class="student">${classe[i].nome} ${classe[i].cognome}</li>`;
-}
-
 
 // Dare la possibilità all’utente, di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+classe = sortListOFStudents(classe);
+printListOfStudents(classe);
 
 document.querySelector("#addStudent").addEventListener('click', () => {
 
+    // validation
+    if ( document.querySelector("#nomeStudente").value == "" ) {
+        alert(`inserisci il nome`);
+        return;
+    } else if ( document.querySelector("#cognomeStudente").value == "" ) {
+        alert(`inserisci il cognome`);
+        return;
+    } else if ( document.querySelector("#etaStudente").value == "" || isNaN(parseInt(document.querySelector("#etaStudente").value)) ) {
+        alert(`inserisci l'età in modo valido`);
+        return;
+    }
+
     // save student data
     let newStudent = {
+        id : classe.length, 
         nome : document.querySelector("#nomeStudente").value,
         cognome : document.querySelector("#cognomeStudente").value,
         eta : document.querySelector("#etaStudente").value
@@ -60,8 +109,11 @@ document.querySelector("#addStudent").addEventListener('click', () => {
     // add student to the class array
     classe.push(newStudent);
 
-    // print on list of students
-    document.querySelector("#listOfStudents").innerHTML += `<li class="student">${newStudent.nome} ${newStudent.cognome}</li>`;
+    // sort by last name
+    classe = sortListOFStudents(classe);
+
+    // print list of students
+    printListOfStudents(classe);
 
     // clear input values
     document.querySelector("#nomeStudente").value = "";
